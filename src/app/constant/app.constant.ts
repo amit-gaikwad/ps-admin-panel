5303714d932aaeb8596f71bdf66107e0dc8c8cd9
Ashish <ashishlagare009@gmail.com>
2018-09-05 20:15:13 +0530

    notice service

--- src/app/constant/app.constant.ts
+++ src/app/constant/app.constant.ts
@@ -2,5 +2,5 @@ export class AppConstant{
     static noticePageLocalJsonUrl= "http://localhost:4200/assets/notice.json";
     static noticePageServerJsonUrl="http://localhost:4200/assets/notice.json";
     static studentUrl = "http://localhost:5000/student";
-    
+    static servarUrl = "http://localhost:5000/";
 }
--- src/app/notice-post/notice-post.component.ts
+++ src/app/notice-post/notice-post.component.ts
@@ -1,59 +1,68 @@
 import { Component } from "@angular/core";
-import { Router,ActivatedRoute,Params } from "@angular/router";
+import { Router, ActivatedRoute, Params } from "@angular/router";
 import { NoticeService } from "../Services/notice.service";
-
+import { Notice } from '../model/notice';
 
 @Component({
-    selector:"app-notice-post",
-    templateUrl:"./notice-post.component.html",
-    styleUrls:["./notice-post.component.css"]
+    selector: "app-notice-post",
+    templateUrl: "./notice-post.component.html",
+    styleUrls: ["./notice-post.component.css"]
 })
-export class NoticePostComponent{
-    selectedNoticeId:number;
-    eNotices=[];
-    nTitle:string;
-    nDescription:string;
-    nDate:Date;
-    nClass:string;
-    classes = ["0-2 Years","2-3 Years","3+ Years"];
-    constructor(private activatedRoute:ActivatedRoute, private noticeService:NoticeService )
-    {
-       activatedRoute.params.subscribe((params:Params)=>{
-            this.selectedNoticeId=Number(params["nId"])
+export class NoticePostComponent {
+    selectedNoticeId: number;
+    eNotices = [];
+    nTitle='';
+    nDescription='';
+    nDate: Date;
+    nClass='';
+    classes = ["0-2 Years", "2-3 Years", "3+ Years"];
+    constructor(private activatedRoute: ActivatedRoute, private noticeService: NoticeService) {
+        activatedRoute.params.subscribe((params: Params) => {
+            this.selectedNoticeId = Number(params["nId"])
         });
         console.log("This is ID", this.selectedNoticeId);
 
         noticeService.getAll().subscribe
-        (
-        (data)=>
-                {
-                this.eNotices=data;
-                var element=this.eNotices.find((item)=>{
-                    return (item.id==this.selectedNoticeId);
+            (
+            (data) => {
+                this.eNotices = data;
+                var element = this.eNotices.find((item) => {
+                    return (item.id == this.selectedNoticeId);
                 })
                 console.log(element);
-                if(this.selectedNoticeId!=0 && this.selectedNoticeId!=null){
-                    this.nTitle=element.title;
-                    this.nDescription=element.description;
-                    this.nDate=element.noticeDate;
-                    this.nClass= element.noticeClass;
-            
-            
+                if (this.selectedNoticeId != 0 && this.selectedNoticeId != null) {
+                    this.nTitle = element.title;
+                    this.nDescription = element.description;
+                    this.nDate = element.noticeDate;
+                    this.nClass = element.noticeClass;
 
-          
+
+
+
                 }
-                }
-        )
+            }
+            )
     }
-    
-    
 
-    onNoticeFormSubmit(value:any){
+    onNoticeFormSubmit(value: any) {
         console.log(value);
-        alert("Notice Posted Sucessfully")
+        var notice = new Notice();
+        notice.title = value.title;
+        notice.description =  value.description ;
+        notice.date = new Date() ;
+        notice.class = value.class ;
+        this.noticeService.create(notice).subscribe(
+            student => {
+              console.log("Notice added successfully");
+            },
+            error => {
+              console.log(error);
+            }
+        );
     }
-    OnChange(value:any)
-    {
+    OnChange(value: any) {
         debugger;
     }
+
+  
 }
