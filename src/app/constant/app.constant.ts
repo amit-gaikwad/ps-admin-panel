1c8c6c0c6f27501727adee07a962b7bfa15a710e
rahul_dudhane <rahul.y.dudhane@gmail.com>
2018-10-04 17:35:59 +0530

    disabled button for parent

--- src/app/constant/app.constant.ts
+++ src/app/constant/app.constant.ts
@@ -1,6 +1,6 @@
 export class AppConstant {
-    // static serverUrl = 'http://54.245.216.148:5000/';
-     static serverUrl = 'http://localhost:5000/';
+     static serverUrl = 'http://54.245.216.148:5000/';
+    // static serverUrl = 'http://localhost:5000/';
 
     static awsAccessKeyId = "AKIAIVBKJ4AK5FNK5M6A";
     static awsSecretAccessKey = "q8MklsOJnOC79x0jLl7zy91C8rHQz8NT0WJqV";
