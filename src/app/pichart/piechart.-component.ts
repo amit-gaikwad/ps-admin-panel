5e18e9b546cdc970cb5542c044d2e8939a925591
Abhi <patilabhijeetb@gmail.com>
2018-10-25 13:19:27 +0530

    Pie Chart

--- src/app/pichart/piechart.-component.ts
+++ src/app/pichart/piechart.-component.ts
@@ -1,5 +1,10 @@
 import {Component} from "@angular/core";
 import { Chart } from "angular-highcharts";
+import { StudentService } from '../Services/student.service';
+import { StudentMarkService } from '../Services/studentmarks';
+import { Student } from '../model/student';
+import { StudentMarks } from '../model/studentmarks';
+import { ActivatedRoute, Params } from '@angular/router';
 @Component({
 
     selector: "pie-chart",
@@ -8,7 +13,16 @@ import { Chart } from "angular-highcharts";
 
 export class PieChartComponent{
     chart : any;
-    constructor(){
+    Physical = 0;
+    Education = 0;
+    Group = 0;
+    Creativity = 0;
+    Communication = 0;
+    studentId="";
+    student : Student   ;
+    studentMarks:StudentMarks;
+    studmarks=[];
+    constructor(private studentService: StudentService, private _ActivatedRoute: ActivatedRoute, private _studentMarksService: StudentMarkService){
 
         this.chart = new Chart({
   
@@ -51,15 +65,54 @@ export class PieChartComponent{
                            sliced: false,
                            selected: true
                           },
-                        ['Physical',   45.0],
-                        ['Education',       26.8],
-                        ['Group',    8.5],
-                        ['Creativity',     6.2],
-                        ['Communication',   0.7]
+                          ['Physical',   this.Physical],
+                          ['Education',this.Education],
+                          ['Group',    this.Group],
+                          ['Creativity', this.Creativity],
+                          ['Communication',  this.Communication]
+  
                     ]
                 }]
         
         });
     
+    }
+    ngOnInit() {
+        this._ActivatedRoute.params.subscribe(( parms :Params) =>{ this.studentId = parms["sId"];});
+
+        if (this.studentId != undefined || this.studentId != null)
+        {
+        this.studentService.getById(this.studentId).subscribe((data) => {
+        this.student = data;
+        });
+        }else{
+        this.studentId = localStorage.getItem("sId");//find parent's student id and assign
+        this.studentService.getById(this.studentId).subscribe((data) => {
+        this.student = data;
+         });
+      }
+    }
+    getMarks()
+    {
+        this.studentService.getAll().subscribe
+             (
+             (data) => {
+                 this.studmarks = data;
+                 var element = this.studmarks.filter((item) => {
+                     return (item._id == this.studentId);
+                 })
+                 //console.log(element);
+                 if (this.studentId != null) {
+                     var studentLatestMarks=element.pop();
+                     this.Physical=studentLatestMarks.phyicalactivity;
+                     this.Group=studentLatestMarks.groupactivity;
+                     this.Education=studentLatestMarks.educational;
+                     this.Creativity=studentLatestMarks.creativity;
+                     this.Communication=studentLatestMarks.communication;
+
+                }
+            }
+        
+        )
     }
 }
