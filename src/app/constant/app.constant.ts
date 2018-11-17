c460777cd46f12bad2542c36a0476df0bf95464c
Amit Gaikwad <amit.gaikwad@weboniselab.com>
2018-11-17 15:00:01 +0530

    port changed

--- src/app/constant/app.constant.ts
+++ src/app/constant/app.constant.ts
@@ -1,13 +1,13 @@
 export class AppConstant {
     //  static serverUrl = 'http://34.213.109.136:5000/';
     // static serverUrl = 'http://localhost:5000/';
-    static serverUrl = 'http://funnydayschildcare.com.au:5000/';
+    static serverUrl = 'https://funnydayschildcare.com.au:5000/';
 
 
-    static awsAccessKeyId = "AKIAIGEI7HGWYMTIV6ZA";
-    static awsSecretAccessKey = "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";
+    static awsAccessKeyId = 'AKIAIGEI7HGWYMTIV6ZA';
+    static awsSecretAccessKey = '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';
     static awsRegion = 'us-east-2';
    // static awsPhotoUrl = "https://"+"s3-us-west-2.amazonaws.com/preschool-angular/";
-    static awsPhotoUrl = "https://s3.us-east-2.amazonaws.com/qmbucketuploadfile/";
+    static awsPhotoUrl = 'https://s3.us-east-2.amazonaws.com/qmbucketuploadfile/';
 
 }
