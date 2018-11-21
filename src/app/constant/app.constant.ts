4767ed2b1e0af6158f0d86464458374870bddd6a
PatilAbhijeetB <41189764+PatilAbhijeetB@users.noreply.github.com>
2018-11-21 18:00:11 +0530

    Update app.constant.ts

--- src/app/constant/app.constant.ts
+++ src/app/constant/app.constant.ts
@@ -4,10 +4,10 @@ export class AppConstant {
     static serverUrl = 'https://funnydayschildcare.com.au:5000/';
 
 
-    static awsAccessKeyId = 'AKIAIGEI7HGWYMTIV6ZA';
-    static awsSecretAccessKey = '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';
+    static awsAccessKeyId = 'AKIAIKLDHXEIV3RKADFA';
+    static awsSecretAccessKey = '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';
     static awsRegion = 'us-east-2';
    // static awsPhotoUrl = "https://"+"s3-us-west-2.amazonaws.com/preschool-angular/";
-    static awsPhotoUrl = 'https://s3.us-east-2.amazonaws.com/qmbucketuploadfile/';
+    static awsPhotoUrl = 'https://s3.us-east-2.amazonaws.com/preschoolaus/';
 
 }
